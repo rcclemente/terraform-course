811016f75a1f5d249871bf5f0dc300b2226619b3
Ryan Clemente <rclemente@zendesk.com>
2020-05-23 14:12:40 +0800

    demo 01 and 08

---
+++ demo-01_variables/vars.tf
@@ -0,0 +1,19 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-03e33c1cefd1d3d74"
+    us-west-2 = "ami-0ee1a20d6b0c6a347"
+    eu-west-1 = "ami-0a4fbfab82b4aee91"
+  }
+}
+
