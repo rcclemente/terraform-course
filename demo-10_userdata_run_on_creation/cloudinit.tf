3e8d5ee66375cac2464558b9801f1c1290ef6269
Ryan Clemente <rclemente@zendesk.com>
2020-05-24 18:42:52 +0800

    fixes

--- demo-10_userdata_run_on_creation/cloudinit.tf
+++ demo-10_userdata_run_on_creation/cloudinit.tf
@@ -2,35 +2,15 @@ data "template_file" "init-script" {
   template = file("scripts/init.cfg")
   vars = {
     REGION = var.AWS_REGION
-<<<<<<< HEAD
-<<<<<<< HEAD
   }
 }
-=======
-  ***REMOVED***
-***REMOVED***
->>>>>>> 986ba1c... add_demos
-=======
-  }
-}
->>>>>>> 78c7374... update_vars_image
 
 data "template_file" "shell-script" {
   template = file("scripts/volumes.sh")
   vars = {
     DEVICE = var.INSTANCE_DEVICE_NAME
-<<<<<<< HEAD
-<<<<<<< HEAD
   }
 }
-=======
-  ***REMOVED***
-***REMOVED***
->>>>>>> 986ba1c... add_demos
-=======
-  }
-}
->>>>>>> 78c7374... update_vars_image
 
 data "template_cloudinit_config" "cloudinit-example" {
   gzip          = false
@@ -40,29 +20,10 @@ data "template_cloudinit_config" "cloudinit-example" {
     filename     = "init.cfg"
     content_type = "text/cloud-config"
     content      = data.template_file.init-script.rendered
-<<<<<<< HEAD
-<<<<<<< HEAD
   }
-=======
-  ***REMOVED***
->>>>>>> 986ba1c... add_demos
-=======
-  }
->>>>>>> 78c7374... update_vars_image
 
   part {
     content_type = "text/x-shellscript"
     content      = data.template_file.shell-script.rendered
-<<<<<<< HEAD
-<<<<<<< HEAD
   }
 }
-=======
-  ***REMOVED***
-***REMOVED***
->>>>>>> 986ba1c... add_demos
-=======
-  }
-}
->>>>>>> 78c7374... update_vars_image
-
