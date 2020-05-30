3053ed952cbb35430def0319e97a7847a52f4ac9
Ryan Clemente <rclemente@zendesk.com>
2020-05-30 18:07:37 +0800

    demo 16

--- demo-16_elastic_load_balancer/elb.tf
+++ demo-16_elastic_load_balancer/elb.tf
@@ -7,46 +7,20 @@ resource "aws_elb" "my-elb" {
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
-<<<<<<< HEAD
-<<<<<<< HEAD
   }
-=======
-  ***REMOVED***
->>>>>>> 986ba1c... add_demos
-=======
-  }
->>>>>>> 78c7374... update_vars_image
   health_check {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
     target              = "HTTP:80/"
     interval            = 30
-<<<<<<< HEAD
-<<<<<<< HEAD
   }
-=======
-  ***REMOVED***
->>>>>>> 986ba1c... add_demos
-=======
-  }
->>>>>>> 78c7374... update_vars_image
 
   cross_zone_load_balancing   = true
   connection_draining         = true
   connection_draining_timeout = 400
   tags = {
     Name = "my-elb"
-<<<<<<< HEAD
-<<<<<<< HEAD
   }
 }
-=======
-  ***REMOVED***
-***REMOVED***
->>>>>>> 986ba1c... add_demos
-=======
-  }
-}
->>>>>>> 78c7374... update_vars_image
 
--- demo-16_elastic_load_balancer/securitygroup.tf
+++ demo-16_elastic_load_balancer/securitygroup.tf
@@ -7,60 +7,26 @@ resource "aws_security_group" "myinstance" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-<<<<<<< HEAD
-<<<<<<< HEAD
   }
-=======
-  ***REMOVED***
->>>>>>> 986ba1c... add_demos
-=======
-  }
->>>>>>> 78c7374... update_vars_image
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-<<<<<<< HEAD
-<<<<<<< HEAD
   }
-=======
-  ***REMOVED***
->>>>>>> 986ba1c... add_demos
-=======
-  }
->>>>>>> 78c7374... update_vars_image
 
   ingress {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.elb-securitygroup.id]
-<<<<<<< HEAD
-<<<<<<< HEAD
   }
 
   tags = {
     Name = "myinstance"
   }
 }
-=======
-  ***REMOVED***
-
-  tags = {
-    Name = "myinstance"
-  ***REMOVED***
-***REMOVED***
->>>>>>> 986ba1c... add_demos
-=======
-  }
-
-  tags = {
-    Name = "myinstance"
-  }
-}
->>>>>>> 78c7374... update_vars_image
 
 resource "aws_security_group" "elb-securitygroup" {
   vpc_id      = aws_vpc.main.id
@@ -71,40 +37,23 @@ resource "aws_security_group" "elb-securitygroup" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-<<<<<<< HEAD
-<<<<<<< HEAD
   }
-=======
-  ***REMOVED***
->>>>>>> 986ba1c... add_demos
-=======
-  }
->>>>>>> 78c7374... update_vars_image
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-<<<<<<< HEAD
-<<<<<<< HEAD
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = "elb"
   }
 }
-=======
-  ***REMOVED***
-  tags = {
-    Name = "elb"
-  ***REMOVED***
-***REMOVED***
->>>>>>> 986ba1c... add_demos
-=======
-  }
-  tags = {
-    Name = "elb"
-  }
-}
->>>>>>> 78c7374... update_vars_image
 
